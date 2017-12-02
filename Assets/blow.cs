4a76b19be4c0f8fdb16bfaf3652d449519274ced
toppinsb96 <toppinsb@gmail.com>
2017-12-02 16:17:28 -0600

    Enemy stuff almost done

---
+++ Assets/blow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blow : MonoBehaviour {
+	public float thrust = 40;
+	public Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		rb.AddForce(transform.forward * thrust);
+	}
+}
---
+++ Assets/enemyController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+	public Transform target;
+	public float speed;
+	public Transform point1;
+	public Transform point2;
+	public Transform point3;
+	public Transform player;
+	public float reactionDist = 10;
+	Vector3 newDir;
+
+	void FixedUpdate()
+	{
+		float step = speed * Time.deltaTime;
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		float dist = Vector3.Distance(player.position, transform.position);
+
+		if (target == point1) {
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+			newDir = Vector3.RotateTowards(transform.position, target.position, step, 0.0f); 
+			if(transform.position == target.position) { target = point2; }
+
+		} else if (target == point2) {
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+			newDir = Vector3.RotateTowards(transform.position, target.position, step, 0.0f); 
+			if (transform.position == target.position) { target = point3; }
+		} else if (target == point3) {
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+			newDir = Vector3.RotateTowards(transform.position, target.position, step, 0.0f); 
+			if (transform.position == target.position) { target = point1; }
+		} else {
+			target = point1;
+		}
+			
+		if (dist < reactionDist) {
+			target = player;
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+			Vector3 newDir = Vector3.RotateTowards(transform.position, target.position, step, 0.0f); 
+			//if (dist < reactionDist) { target = point1; }
+		}
+
+		transform.rotation = Quaternion.LookRotation(newDir);
+	}
+}
