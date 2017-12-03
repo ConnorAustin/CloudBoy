fde9741a072aadc73078d67514488159a5a235aa
toppinsb96 <toppinsb@gmail.com>
2017-12-02 19:03:28 -0600

    Finished Blowing

---
+++ Assets/testScripts/heightlimiter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heightlimiter : MonoBehaviour {
+	// Update is called once per frame
+	public float originalz, originaly, originalx;
+
+	void Start() {
+		originalx = transform.position.x;
+		originaly = transform.position.y;
+		originalz = transform.position.z;
+	}
+
+	void FixedUpdate () {
+		if(transform.position.y > originaly) {
+			transform.position = new Vector3(originalx, originaly, originalz);
+		}
+	}
+}
